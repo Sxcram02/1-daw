b9ad98c3b7625db18153a0f26df658afa7f84fe6
MarcosDominguezVega <228@cifpceuta.es>
2023-02-07 09:40:34 +0100

    nuevo

--- clases/PROG/Examen/ejecicio-array1-marcos.php
+++ clases/PROG/Examen/ejecicio-array1-marcos.php
@@ -1,15 +1,16 @@
 <?php 
-	define('num',(rand(1,10000));
 	for($i=0;$i<=5;$i++){
 		$array[$i] ='num';
 		echo $array[$i];
 	};
-	for($j=5;$j<=1;$j--;){
+
+	for($j=5;$j<=1;$j--){
 		$array[$j] = $array2;
 		$array[0] = $array2[5];
+		
 		for($l=0;$l<=5;$l++){
 			if($array[$l]==$array2[$l]){
-			echo $array[$l], "el numero es capricua"
+			echo $array[$l], "el numero es capricua";
 		};
 	};
 ?>
