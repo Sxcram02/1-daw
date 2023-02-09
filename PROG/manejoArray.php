f7f9d68306e63b8aa0383b995041c327a50eba32
MarcosDominguezVega <228@cifpceuta.es>
2023-02-09 12:25:00 +0100

    Repasar Programacion

--- PROG/manejoArray.php
+++ PROG/manejoArray.php
@@ -1,13 +1,29 @@
 <?php
-    for($i=0;$i<=10;$i++){
-        $array1[$i] = $i;
-        echo "$array1[$i]"." ";
-    };
-    $c1 = 0;
+    //EJERCICIO 1
+    echo "<br />";
 
-    for($j=10;$j=1;$j--){
-        $array2[$j] = $c1 + 1;
-        echo "$array2[$j]"." ";
+    for($i=0;$i<11;$i++){
+    $array[$i]=0;
+    echo $array[$i] . " ";
     };
+
+    //EJERCICIO 2
+echo "<br />";
+
+$j=0;
+while (!$j=10) {
+    $num=$j;
+    $array2[$j]= $num;
+    echo $array2[$j]." ";
+    $j=$j+1;
+}
+;
+for ($c=10;$i=1;$i--){
+    echo $array2[$c];
+}
+;
+    //EJERCICIO 3
+    echo "<br />";
+
 
 ?>
